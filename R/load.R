6e0b9e088cc49e506bcd3630cddc12785b19daa1
John Verzani <jverzani@gmail.com>
2012-05-29 22:32:12 -0400

    remove brew and authenticator for now

--- R/load.R
+++ R/load.R
@@ -73,28 +73,20 @@ R_http <- setRefClass("R_http",
                                                     app_name=gsub("\\..*", "", basename(script_name)),
                                                     session_manager=make_session_manager(),
                                                     open_page=TRUE,
-                                                    brew_template = "",
                                                     show.log=FALSE,
-                                                    authenticator=NULL,
+                                                    webpage = NULL, 
+#                                                    authenticator=NULL,
                                                     ...)  {
 
                                                     options("Rhttpd_debug"=as.logical(show.log))
 
                                                     ## Make the page
-                                                    page <-WebPage$new(url="/", app_name=app_name, ...)
-                                                    ## how to subclass to put in brew_template
-                                                    if(file.exists(brew_template)) {
-                                                      BrewPage <- setRefClass("BrewPage",
-                                                                              contains="WebPage",
-                                                                              fields=c("brew_template"),
-                                                                              methods=list(
-                                                                                body=function() {
-                                                                                  brew_template
-                                                                                }
-                                                                                ))$new(url="/", app_name=app_name,
-                                                                                       renderer="brew", brew_template=brew_template)
-                                                    }
-                                                    R$add(RhttpdApp$new(page, name=app_name))
+                                                    if(is.null(webpage))
+                                                      webpage <-WebPage$new(url="/", app_name=app_name, ...)
+                                                    else
+                                                      webpage$app_name <- app_name
+                                                  
+                                                    R$add(RhttpdApp$new(webpage, name=app_name))
                                                     
                                                     message(list("*** making app ***", app_name))
                                                     
@@ -119,8 +111,6 @@ R_http <- setRefClass("R_http",
 ##' @param dirname The directory name (with full path). We make several assuptions:
 ##' 1) `dir/*.R` has one answer, or we take `index.R`, or we take first alphabetically;
 ##' 2) `app_name` is `basename(dir_name)`;
-##' 3) `brew/*rhtml` is empty (fullscreen), has one value, or has index.rhtml` or we take first alphabetically;
-##' 4) authenticator (XXX to be determined, for now calls `make_session_manager`)
 ##' @export
 ##' @return creates the app
 "
@@ -139,13 +129,6 @@ R_http <- setRefClass("R_http",
                                                       r_file <- ifelse(is.na(ind), r_files[1], r_files[ind])
                                                     }
                                                     
-                                                    brew_files <- Sys.glob(sprintf("%s%sbrew%s*.rhtml", dir_name, sep, sep))
-                                                    if(length(brew_files) <= 1) {
-                                                      brew_file <- ""
-                                                    } else {
-                                                      ind <- match("index.rhtml", basename(brew_files))
-                                                      brew_file <- ifelse(is.na(ind), brew_files[1], brew_files[ind])
-                                                    }
 
                                                     ## How to do make_session
                                                     ## how to do Authenticator
@@ -155,7 +138,6 @@ R_http <- setRefClass("R_http",
                                                     load_app(
                                                              script_name=r_file,
                                                              app_name=app_name,
-                                                             brew_template = brew_file,
                                                              ...
                                                              )
                                                     
@@ -186,55 +168,28 @@ r_httpd <- R_http$get_instance()
 ##' its own environment in the R session, so running lots of instances
 ##' can be expensive. That is, don't expect gWidgetsWWW2 to scale.
 ##'
-##' There are other cases one might want. An app can run within a web
-##' page. The web template can be passed through the
-##' \code{brew_template} argument with the \code{...} parameters
-##' passed along. Pass a DOM id to the \code{renderTo} argument of the
-##' \code{gwindow} instance.
+##' To embed an app within a web page, use an \code{iframe} tag.
 ##'
-##' Apps can require authentification. By default, this is not
-##' validated. One can write a subclass of \code{Authenticator} to do
-##' other validations. (Of course this needs improvement).
-##' 
 ##' @param script_name path to gWidgetssWWW2 script
 ##' @param app_name base name for script, unique per project
-##' @param brew_template The script may render to the entire page, or
-##' parts of the page specified by an ID. The allows one to specify a
-##' brew template to hold these place holders and other HTML code.
 ##' @param show.log If TRUE, logged information is written to the console
 ##' @param port Initial port for Rhttpd server, provided it hasn't already started.
-##' @param authenticator Used to specify a subclass of
-##' \code{\link{Authenticator-class}} for doing authentification. Specifying
-##' a value of \code{TRUE} will do a trivial
-##' authentification. Subclasses of \code{Authenticator} should
-##' implement the methods \code{is_valid_cookie} and
-##' \code{is_valid_user}.
 ##' @param session_manager an instance of \code{make_session_manager}
 ##' @param open_page logical. If \code{TRUE} call \code{browseURL} to open the app.
-##' @param ... Passed to brew call when a template is given. As well,
-##' passed to WebPage object. Named arguments \code{body} and
-##' \code{head} can be used to insert information into each, such as
-##' loading of style sheets etc. through \code{head}.
+##' @param ... Passed to WebPage object. Named arguments
+##' \code{body} and \code{head} can be used to insert information into
+##' each, such as loading of style sheets etc. through \code{head}. One can also subclass \code{WebPage} and pass this in via \code{webpage}.
 ##' @export
 ##' @examples
 ##' ## open an app that takes the entire page
 ##' gw_script <-  system.file("examples/hello-world.R", package="gWidgetsWWW2")
 ##' if(interactive()) load_app(gw_script, "HelloApp")
-##' ## open an app using googlemaps
-##' if(interactive()) load_app(system.file("examples/ggooglemaps.R", package="gWidgetsWWW2"),  "GoogleMapApp")
-##' ## open an app embedded in another page
-##' gw_script <- system.file("examples/ex-multiple-gwindow.R", package="gWidgetsWWW2")
-##' brew_template <- system.file("framework/brew/custom.rhtml", package="gWidgetsWWW2")
-##' if(interactive()) load_app(gw_script, "MultipleApp",  brew_template)
-##' 
 load_app <- function(script_name,
                      app_name=gsub("\\..*", "", basename(script_name)),
                      port=9000L,
                      session_manager=make_session_manager(),
                      open_page=TRUE,
-                     brew_template = "",
                      show.log=FALSE,
-                     authenticator=NULL,
                      ...
                          ) {
 
@@ -242,7 +197,9 @@ load_app <- function(script_name,
   r_httpd$start(port)                   # if not started
   r_httpd$load_gw(session_manager)
   ## now load app
-  r_httpd$load_app(script_name, app_name, session_manager, open_page, brew_template, show.log, authenticator, ...)
+  r_httpd$load_app(script_name, app_name, session_manager, open_page,  show.log,
+                   ##authenticator,
+                   ...)
 }
 
 ##' Load an app in a directory
